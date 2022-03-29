0883dfe47a8f6075d92e16eb9c0df676363a4f2e
nelsonaq <chibuds217@gmail.com>
2022-03-29 09:56:08 +0800

    added documentation to the functions

--- script.js
+++ script.js
@@ -13,20 +13,30 @@ let ms = 0;
 let min = 0;
 let timerMilliseconds;
 
+/**
+ * Renders the value in the DOM
+ */
 const updateUI = function () {
   milliseconds.textContent = `${ms}`.slice(-2).padStart(2, "0");
   seconds.textContent = `${sec}`.padStart(2, "0");
   minutes.textContent = `${min}`.padStart(2, "0");
 };
 
+/**
+ * Switches the button from Start to Stop and vice versa
+ * @param {HTMLElement} display - gets displayed in the DOM
+ * @param {HTMLElement} hide - gets hidden in the DOM
+ */
 const btnSwapDisplay = function (display, hide) {
   display.style.display = "block";
   hide.style.display = "none";
 };
 
+/**
+ * Starts the timer.
+ */
 const timerStart = function () {
   btnSwapDisplay(btnStop, btnStart);
-  clearInterval(timerMilliseconds);
   timerMilliseconds = setInterval(() => {
     if (ms === 100) {
       ms = 0;
@@ -41,23 +51,31 @@ const timerStart = function () {
   }, 10);
 };
 
+/**
+ * Stops the timer
+ */
 const timerStop = function () {
   clearInterval(timerMilliseconds);
   btnSwapDisplay(btnStart, btnStop);
 };
 
+/**
+ * Resets the timer back to zero
+ */
 const timerReset = function () {
   clearInterval(timerMilliseconds);
   btnSwapDisplay(btnStart, btnStop);
-  sec = 0;
-  ms = 0;
-  min = 0;
+  ms = sec = min = 0;
   updateUI();
 };
 
 let num = 0;
+/**
+ * Renders current time in the DOM
+ * @returns {undefined} Returns if current time displayer is zero.
+ */
 const bookmarkTime = function () {
-  if (!ms && !s) return;
+  if (!ms) return;
   num++;
   const markup = `
     <div class="lap-list">
@@ -70,6 +88,9 @@ const bookmarkTime = function () {
   lapContainer.insertAdjacentHTML("beforeend", markup);
 };
 
+/**
+ * Clears the list of laps bookmarked
+ */
 const clearBookmark = function () {
   num = 0;
   lapContainer.innerHTML = "";
